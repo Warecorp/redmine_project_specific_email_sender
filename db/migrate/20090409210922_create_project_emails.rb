aeb436721b9d8a53a95044bc0ecaaaa0261dd8c2
Sergey Yelenski <sergey.yelenski@warecorp.com>
2021-02-03 11:04:19 +0300

    Migration changed

--- db/migrate/20090409210922_create_project_emails.rb
+++ db/migrate/20090409210922_create_project_emails.rb
@@ -4,6 +4,7 @@ class CreateProjectEmails < ActiveRecord::Migration[5.2]
       t.string :email
       t.references :project
     end
+    remove_index :project_emails, :project_id #:project_emails, name: "index_project_emails_on_project_id"
     add_index :project_emails, :project_id
   end
 
